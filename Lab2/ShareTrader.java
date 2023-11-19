5263264be8777627f3bdafae488a2c820b35c4ff
AnushkaMazumdar2 <140642505+AnushkaMazumdar2@users.noreply.github.com>
2023-11-20 01:01:37 +0530

    Update ShareTrader.java

--- Lab2/ShareTrader.java
+++ Lab2/ShareTrader.java
@@ -4,36 +4,49 @@ public class ShareTrader
 static int maxProfit;
 public static void findMaxProfit(int[] price)
 {
-int[] buy = new int[2];
-int[] sell = new int[2];
-buy[1] = price[0];
-sell[1] = price[1];
-int profits;
-profits = sell[1]-buy[1];
-maxProfit += profits;
-int min = price[2];
-int max = price[2];
-for(int i =2;i<price.length;i++)
+     maxProfit = 0;
+    int n = price.length;
+    int[] profit = new int[n];
+    int min = price[0];
+for(int i = 1;i<n;i++)
 {
-if(price[i]<min)
+        min = Math.min(min,price[i]);
+        profit[i] = Math.max(profit[i-1],price[i]-min);
+}
+int max = price[n-1];
+int maxProfit = profit[n-1];    
+for(int i=n-2;i>0;i--)
 {
-min = price[i];
+    max = Math.max(max,price[i]);
+    maxProfit= Math.max(maxProfit,profit[i-1]+max -price[i]);
 }
-else if(price[i]>max)
-{
-max = price[i];
-}
-}
-int profit = max - min;
-maxProfit += profit;
 System.out.println(maxProfit);
-    
 }
-
+public static boolean stock(int[] price)
+{
+    boolean value = true;
+    for(int i=0;i<price.length-1;i++)
+    {
+        if(price[i]<=price[i+1])
+        {
+        value = false;
+        break;
+        }
+    }
+    return value;
+}
 public static void main(String args[])
 {
     ShareTrader obj = new ShareTrader();
-    int arr[] = {2, 30, 15, 10, 8, 25, 80};
-    obj.findMaxProfit(arr);
+    int arr[] = {10, 22, 5, 75, 65, 80};
+    boolean result = stock(arr);
+    if(result==true)
+    {
+        System.out.println("All the stock prices are decreasing in the day. No way to earn");
+    }
+    else
+    {
+        obj.findMaxProfit(arr);
+    }
 }
 }
